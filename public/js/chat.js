cfa9337689cc3fe57472373169e0776cea25ce73
Lukas Vrajik <lukas.vrajik@zonky.cz>
2019-10-02 19:54:11 +0200

    MVP delete only own messages

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,6 +1,17 @@
+const webToken = 'CBCLUBZLIN_CHAT_TOKEN';
+
 class Chat {
     constructor() {
         this._showMessages();
+        this._webToken = window.localStorage.getItem(webToken);
+        if (!this._webToken) {
+            window.localStorage.setItem(webToken, this._generateToken());
+            this._webToken = window.localStorage.getItem(webToken);
+        }
+    }
+
+    _generateToken() {
+        return Math.random().toString(36).substr(2) + Math.random().toString(36).substr(2);
     }
 
     async _loadMessages() {
@@ -10,9 +21,15 @@ class Chat {
 
     async _showMessages() {
         const messages = await this._loadMessages();
-        for (var message of messages) {
-            const line = this.createLineDOMElement(message);
-            document.getElementById("messages").appendChild(line);
+        if (messages.length > 0) {
+            for (var message of messages) {
+                const line = this.createLineDOMElement(message);
+                document.getElementById("messages").appendChild(line);
+            }
+        } else {
+            const emptyMessage = document.createElement('p');
+            emptyMessage.innerHTML = "Místo pro zanechání vzkazu. Zatím tu žádný není.";
+            document.getElementById("messages").appendChild(emptyMessage);
         }
     }
 
@@ -24,13 +41,14 @@ class Chat {
     createLineDOMElement(lineData) {
         const line = document.createElement('div');
         line.className = 'message';
-
-        const deleteButton = document.createElement('button');
-        deleteButton.name = lineData['id'];
-        deleteButton.innerHTML = 'Smazat';
-        deleteButton.className = 'message__delete-button';
-        deleteButton.onclick = (e) => CHAT.deleteMessage(e);
-        line.appendChild(deleteButton);
+        if (lineData.webToken === this._webToken) {
+            const deleteButton = document.createElement('button');
+            deleteButton.name = lineData['id'];
+            deleteButton.innerHTML = 'Smazat';
+            deleteButton.className = 'message__delete-button';
+            deleteButton.onclick = (e) => CHAT.deleteMessage(e);
+            line.appendChild(deleteButton);
+        }
 
         const date = document.createElement('div');
         date.innerHTML = new Date(lineData['date']).toLocaleDateString();
@@ -55,7 +73,7 @@ class Chat {
         const author = context.form[0].value;
         const message = context.form[1].value;
         if (author && message) {
-            const data = { author, message };
+            const data = { author, message, webToken: this._webToken };
             fetch('api/message', {
                 method: 'POST', body: JSON.stringify(data), headers: {
                     'Content-Type': 'application/json'
